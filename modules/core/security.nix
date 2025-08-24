f50861314055395acf64f3dfcf52cc7728d31384
JJ_jako <79741143+JJjako@users.noreply.github.com>
2025-08-24 15:29:11 +0200

    security.nix aktualisieren

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   security.rtkit.enable = true;
-  security.sudo.enable = false;
+  security.sudo.enable = true;
   security.pam.services.swaylock = { };
   security.pam.services.hyprlock = { };
 }
