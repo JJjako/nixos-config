127e9ec787bfd2e7b680abeb3cca32bce975223c
JJjako <jakob.wiinkler@gmail.com>
2025-10-01 18:09:57 +0200

    Update NixOS config (desktop) on 2025-10-01 18:09:57

--- modules/home/scripts/scripts/wallpaper-picker.sh
+++ modules/home/scripts/scripts/wallpaper-picker.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-wallpaper_path=$HOME/Pictures/wallpapers
-wallpapers_folder=$HOME/Pictures/wallpapers/others
-wallpaper_name="$(ls $wallpapers_folder | rofi -dmenu || pkill rofi)"
+wallpaper_path="$HOME/Pictures/wallpapers"
+wallpapers_folder="$HOME/Pictures/wallpapers/others"
 
-if [[ -f $wallpapers_folder/$wallpaper_name ]]; then
-    ln -sf "$wallpapers_folder/$wallpaper_name" "$wallpaper_path/wallpaper"
+# Rekursiv alle Bilddateien finden
+mapfile -t wallpaper_list < <(find "$wallpapers_folder" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \))
+
+# Mit rofi auswählen
+wallpaper_file=$(printf '%s\n' "${wallpaper_list[@]}" | rofi -dmenu -i || pkill rofi)
+
+if [[ -f "$wallpaper_file" ]]; then
+    ln -sf "$wallpaper_file" "$wallpaper_path/wallpaper"
     wall-change "$wallpaper_path/wallpaper"
 else
     exit 1
