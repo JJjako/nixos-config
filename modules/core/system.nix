aadedf7942c8297f7b69b5d0be68a4837251520a
JJjako <jakob.wiinkler@gmail.com>
2025-10-01 17:38:29 +0200

    Update NixOS config (desktop) on 2025-10-01 17:38:29

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -27,6 +27,10 @@
   };
 
   environment.systemPackages = with pkgs; [
+    python3
+    python3Packages.jupyterlab
+    python3Packages.networkx
+    python3Packages.matplotlib
     teams-for-linux
     wtype
     virtualbox    
