3495757439863100bc4a5a2b125ecd362058417c
JJjako <jakob.wiinkler@gmail.com>
2025-10-01 18:08:01 +0200

    Update NixOS config (desktop) on 2025-10-01 18:08:01

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 init() {
     # Vars
-    CURRENT_USERNAME='frostphoenix'
+    CURRENT_USERNAME='JJjako'
 
     # Colors
     NORMAL=$(tput sgr0)
--- modules/home/scripts/scripts/random-wallpaper.sh
+++ modules/home/scripts/scripts/random-wallpaper.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
 wallpaper_path="$HOME/Pictures/wallpapers"
-wallpapers_folder="$HOME/Pictures/wallpapers/Clean"
+wallpapers_folder="$HOME/Pictures/wallpapers/gruvbox-wallpapers"
 
 current_wallpaper=$(readlink "$wallpaper_path/wallpaper" 2> /dev/null)
 current_wallpaper_name="$(basename "$current_wallpaper")"
 
-wallpaper_list=($(ls "$wallpapers_folder"))
+# Find all images recursively (jpg, png, jpeg, etc.)
+mapfile -t wallpaper_list < <(find "$wallpapers_folder" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \))
+
 wallpaper_count=${#wallpaper_list[@]}
 
 while true; do
-    wallpaper_name="${wallpaper_list[RANDOM % wallpaper_count]}"
+    wallpaper_file="${wallpaper_list[RANDOM % wallpaper_count]}"
+    wallpaper_name="$(basename "$wallpaper_file")"
 
     if [[ "$wallpaper_name" != "$current_wallpaper_name" ]]; then
         break
     fi
 done
 
-ln -sf "$wallpapers_folder/$wallpaper_name" "$wallpaper_path/wallpaper"
+ln -sf "$wallpaper_file" "$wallpaper_path/wallpaper"
 wall-change "$wallpaper_path/wallpaper" &
