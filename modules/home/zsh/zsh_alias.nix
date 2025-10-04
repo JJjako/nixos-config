09e0e8e8e1eaf231ccb03d3c6e98fcc4ad52dd1c
JJjako <jakob.wiinkler@gmail.com>
2025-10-04 18:53:06 +0200

    Update NixOS config (desktop) on 2025-10-04 18:53:06

--- modules/home/zsh/zsh_alias.nix
+++ modules/home/zsh/zsh_alias.nix
@@ -28,13 +28,13 @@
       tree = "eza --icons --tree --group-directories-first";
 
       
-      ns = "nom-shell --run zsh";
-      nd = "nom develop --command zsh";
-      nb = "nom build";
-      nc = "nh clean all --keep 5";
-      nft = "nh os test";
-      nfs = "nh os switch";
-      nfu = "nh os switch --update";
+      #ns = "nom-shell --run zsh";
+      #nd = "nom develop --command zsh";
+      #nb = "nom build";
+      #nc = "nh clean all --keep 5";
+      #nft = "nh os test";
+      #nfs = "nh os switch";
+      #nfu = "nh os switch --update";
       # nix-search = "nh search";
 
       # python
