3f5f31a09c129350d12479fcd769e575cffa6947
JJjako <jakob.wiinkler@gmail.com>
2025-10-09 12:42:21 +0200

    Update NixOS config (laptop) on 2025-10-09 12:42:21

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  efi = builtins.pathExists "/sys/firmware/efi";
+in
 {
-  boot.loader.grub.enable = false;
-  boot.loader.grub.device = "dev/sda";
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.supportedFilesystems = [ "fat" ];
+  boot.loader = if efi then {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  } else {
+    grub.enable = true;
+    grub.device = "/dev/sda";
+  };
 }
