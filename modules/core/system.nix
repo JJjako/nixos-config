d4fa6a02cfa16cdc7eeb5e9f576a1d8636b363e0
JJjako <jakob.wiinkler@gmail.com>
2025-11-09 10:43:58 +0100

    Update NixOS config () on 2025-11-09 10:43:58

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -37,7 +37,7 @@
   environment.variables = {
      USED_EDITOR = "subl";};
   environment.systemPackages = with pkgs; [
-    pycharm-community
+    jetbrains.pycharm-community
     docker
     prismlauncher
     quickemu
