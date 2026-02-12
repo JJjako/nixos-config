062815226f9e85490fff10373f22bfad95983b05
JJjako <jakob.wiinkler@gmail.com>
2026-02-12 10:37:29 +0100

    Update NixOS config () on 2026-02-12 10:37:29

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -26,7 +26,7 @@ in
 
         "${terminal} --gtk-single-instance=true --quit-after-last-window-closed=false --initial-window=false"
         
-  		  "[workspace 0 silent] kitty -d ~/shared/untis-google-calendar-sync -e "nix-shell""
+  		  "[workspace 0 silent] (kitty -d ~/shared/untis-google-calendar-sync -e "nix-shell")"
       ];
 
       input = {
