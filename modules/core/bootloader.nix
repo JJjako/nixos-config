b1e82de8d621c236d0f5dd3010470a49e0395852
JJjako <jakob.wiinkler@gmail.com>
2026-03-28 23:04:45 +0100

    Update bootloader

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -8,11 +8,7 @@ in {
     systemd-boot.editor = false;
 
     # Desktop → EFI (systemd-boot)
-    systemd-boot.enable = lib.mkIf (hostname == "desktop") true;
-    efi.canTouchEfiVariables = lib.mkIf (hostname == "desktop") true;
-
-    # Laptop → BIOS (grub)
-    grub.enable = lib.mkIf (hostname == "laptop") true;
-    grub.device = lib.mkIf (hostname == "laptop") "/dev/sda";
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
   };
 }
