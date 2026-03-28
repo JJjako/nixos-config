d5cc5b2d87c67b35aebea975d28ad1afae3c8c8d
JJjako <jakob.wiinkler@gmail.com>
2026-03-28 23:51:06 +0100

    Mehrere Sprachen togglebbar

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -29,8 +29,8 @@ in
       ];
 
       input = {
-        kb_layout = "us";
-        #kb_options = "grp:alt_caps_toggle";
+        kb_layout = "us,de";
+        kb_options = "grp:alt_caps_toggle";
         numlock_by_default = true;
 	        repeat_delay = 300;
         follow_mouse = 0;
