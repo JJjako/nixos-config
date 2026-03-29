86b7df7c64fcc3e68c7ab32a24d9a8b5789d7a19
JJjako <jakob.wiinkler@gmail.com>
2026-03-29 09:58:31 +0200

    Update NixOS config () on 2026-03-29 09:58:31

--- modules/home/custom/pythonpkgs.nix
+++ modules/home/custom/pythonpkgs.nix
@@ -1,7 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
 environment.systemPackages = with pkgs; [
    python3
     python3Packages.jupyterlab
     python3Packages.networkx
     python3Packages.matplotlib
+    qlcplus
+    
     ]}
