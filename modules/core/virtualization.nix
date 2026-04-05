85dab5aff471e79921e5261d6c8895b76b2cafc6
JJjako <jakob.wiinkler@gmail.com>
2026-04-05 11:01:06 +0200

    Update NixOS config () on 2026-04-05 11:01:06

--- modules/core/virtualization.nix
+++ modules/core/virtualization.nix
@@ -1,9 +1,6 @@
 { pkgs, username, ... }:
 {
-  # Add user to libvirtd group
   users.users.${username}.extraGroups = [ "libvirtd" ];
-
-  # Install necessary packages
   environment.systemPackages = with pkgs; [
     virt-manager
     virt-viewer
@@ -14,15 +11,11 @@
     win-spice
     adwaita-icon-theme
   ];
-
-  # Manage the virtualisation services
   virtualisation = {
     libvirtd = {
       enable = true;
       qemu = {
         swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
       };
     };
     spiceUSBRedirection.enable = true;
