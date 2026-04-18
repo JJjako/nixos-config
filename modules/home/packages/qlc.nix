ee2c576818b1d6f200e6a3843d32c60d2f5cfcec
JJjako <jakob.wiinkler@gmail.com>
2026-04-18 17:59:29 +0200

    Update NixOS config () on 2026-04-18 17:59:29

--- modules/home/packages/qlc.nix
+++ modules/home/packages/qlc.nix
@@ -1,13 +1,5 @@
-{ pkgs, inputs, ... }:
-let
-  launchpad-control = pkgs.rustPlatform.buildRustPackage {
-    pname = "launchpad-control";
-    version = "0.1.0";
-    src = inputs.midi-qlc-src;
-    cargoLock.lockFile = "${inputs.midi-qlc-src}/Cargo.lock";
-    buildInputs = with pkgs; [ alsa-lib ];
-    nativeBuildInputs = with pkgs; [ pkg-config ];
-  };
-in {
-  home.packages = [ launchpad-control ];
-}
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+  go
+  ];}
