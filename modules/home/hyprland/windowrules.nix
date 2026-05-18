84c5a0fc6dd0d08803b8092ed197f8a585ee9586
JJjako <jakob.wiinkler@gmail.com>
2026-05-18 18:32:06 +0200

    Update NixOS config () on 2026-05-18 18:32:06

--- modules/home/hyprland/windowrules.nix
+++ modules/home/hyprland/windowrules.nix
@@ -1,5 +1,6 @@
 { ... }:
 {
+  {
   wayland.windowManager.hyprland.settings = {
     windowrule = [
       "float, class:^(imv)$"
@@ -10,20 +11,14 @@
       "float, class:^(org.gnome.Calculator)$"
       "float, class:^(org.gnome.FileRoller)$"
       "float, class:^(org.pulseaudio.pavucontrol)$"
-
       "pin, class:^(rofi)$"
       "pin, class:^(waypaper)$"
-
       "tile, class:^(Aseprite)$"
-
       "size 850 500, class:^(zenity)$"
-
       "size 700 450, title:^(Volume Control)$"
       "move 40 55%, title:^(Volume Control)$"
-
       "pin, title:^(Picture-in-Picture)$"
       "float, title:^(Picture-in-Picture)$"
-
       "workspace 1, class:^(zen-beta)$"
       "workspace 3, class:^(codium)$"
       "workspace 4, class:^(Gimp-2.10)$"
@@ -34,20 +29,18 @@
       "workspace 10, class:^(discord)$"
       "workspace 10, class:^(WebCord)$"
       "workspace 10, class:^(vesktop)$"
-
-     
-
+    ];
+    windowrulev2 = [
       "rounding 0, xwayland:1"
-
       "bordersize 0, floating:0, workspace:w[tv1]"
       "rounding 0, floating:0, workspace:w[tv1]"
       "bordersize 0, floating:0, workspace:f[1]"
       "rounding 0, floating:0, workspace:f[1]"
     ];
-
     workspace = [
       "w[tv1], gapsout:0, gapsin:0"
       "f[1], gapsout:0, gapsin:0"
     ];
   };
+}
 }
